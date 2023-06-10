873f3c2c8d2d3fbca31593c0ce98d5079ce1693a
mizuki <apple@DESKTOP-GMQFFT0>
2023-06-10 11:31:45 +0900

    ha

--- hackathon/main.go
+++ hackathon/main.go
@@ -44,95 +44,84 @@ func init() {
 	db = _db
 }
 
-func postMessage(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	var message Message
-	err := json.NewDecoder(r.Body).Decode(&message)
-	if err != nil {
-		log.Printf("fail: json.NewDecoder, %v\n", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if message.Content == "" {
-		log.Println("fail: content is empty")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	t := time.Now()
-	message.ID = generateID()
-	message.CreatedAt = t
-	message.ModifiedAt = t
-
-	_, err = db.Exec("INSERT INTO message (id, content, channel_id, created_at, modified_at) VALUES (?, ?, ?, ?, ?)",
-		message.ID, message.Content, message.ChannelID, message.CreatedAt, message.ModifiedAt)
-	if err != nil {
-		log.Printf("fail: db.Exec, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getMessages(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	channelID := r.URL.Query().Get("channel_id")
-	if channelID == "" {
-		log.Println("fail: channel_id is empty")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	rows, err := db.Query("SELECT id, content, channel_id, created_at, modified_at FROM message WHERE channel_id = ?", channelID)
-	if err != nil {
-		log.Printf("fail: db.Query, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	messages := make([]Message, 0)
-	for rows.Next() {
-		var message Message
-		var createdAt, modifiedAt string
-		if err := rows.Scan(&message.ID, &message.Content, &message.ChannelID, &createdAt, &modifiedAt); err != nil {
-			log.Printf("fail: rows.Scan, %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		message.CreatedAt, _ = time.Parse("2006-01-02 15:04:05", createdAt)
-		message.ModifiedAt, _ = time.Parse("2006-01-02 15:04:05", modifiedAt)
-
-		messages = append(messages, message)
-	}
-
-	bytes, err := json.Marshal(messages)
-	if err != nil {
-		log.Printf("fail: json.Marshal, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(bytes)
-}
-
-func editMessage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	switch r.Method {
 	case http.MethodOptions:
 		w.Header()
+
+	case http.MethodPost:
+		var message Message
+		err := json.NewDecoder(r.Body).Decode(&message)
+		if err != nil {
+			log.Printf("fail: json.NewDecoder, %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if message.Content == "" {
+			log.Println("fail: content is empty")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		t := time.Now()
+		message.ID = generateID()
+		message.CreatedAt = t
+		message.ModifiedAt = t
+
+		_, err = db.Exec("INSERT INTO message (id, content, channel_id, created_at, modified_at) VALUES (?, ?, ?, ?, ?)",
+			message.ID, message.Content, message.ChannelID, message.CreatedAt, message.ModifiedAt)
+		if err != nil {
+			log.Printf("fail: db.Exec, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+
+	case http.MethodGet:
+		channelID := r.URL.Query().Get("channel_id")
+		if channelID == "" {
+			log.Println("fail: channel_id is empty")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		rows, err := db.Query("SELECT id, content, channel_id, created_at, modified_at FROM message WHERE channel_id = ?", channelID)
+		if err != nil {
+			log.Printf("fail: db.Query, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		messages := make([]Message, 0)
+		for rows.Next() {
+			var message Message
+			var createdAt, modifiedAt string
+			if err := rows.Scan(&message.ID, &message.Content, &message.ChannelID, &createdAt, &modifiedAt); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			message.CreatedAt, _ = time.Parse("2006-01-02 15:04:05", createdAt)
+			message.ModifiedAt, _ = time.Parse("2006-01-02 15:04:05", modifiedAt)
+
+			messages = append(messages, message)
+		}
+
+		bytes, err := json.Marshal(messages)
+		if err != nil {
+			log.Printf("fail: json.Marshal, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
 
 	case http.MethodPut:
 		var message Message
@@ -167,33 +156,28 @@ func editMessage(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusOK)
 
+	case http.MethodDelete:
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			log.Println("fail: id is empty")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		_, err := db.Exec("DELETE FROM message WHERE id = ?", id)
+		if err != nil {
+			log.Printf("fail: db.Exec, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
 	default:
 		log.Printf("fail: HTTP Method is %s\n", r.Method)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-}
-
-func deleteMessage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		log.Println("fail: id is empty")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	_, err := db.Exec("DELETE FROM message WHERE id = ?", id)
-	if err != nil {
-		log.Printf("fail: db.Exec, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func generateID() string {
@@ -204,10 +188,7 @@ func generateID() string {
 }
 
 func main() {
-	http.HandleFunc("/message", postMessage)
-	http.HandleFunc("/messages", getMessages)
-	http.HandleFunc("/message/edit", editMessage)
-	http.HandleFunc("/message/delete", deleteMessage)
+	http.HandleFunc("/message", handler)
 	closeDBWithSysCall()
 	log.Println("Listening...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
